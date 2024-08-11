c88a2defeb1cde1cb87bb8f5f340f082e95ed56f
non-97 <nonmonaka07@gmail.com>
2024-08-11 11:06:52 +0900

    Initial commit

---
+++ test/mountpoint-s3-sftp.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MountpointS3Sftp from '../lib/mountpoint-s3-sftp-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/mountpoint-s3-sftp-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MountpointS3Sftp.MountpointS3SftpStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
